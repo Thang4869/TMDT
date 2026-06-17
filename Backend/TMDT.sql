5852e1df2c3a18cce2878baab97ab45fe9de272d
Thang4869 <vanthang48692109@gmail.com>
2026-06-18 00:49:00 +0700

    :card_file_box: Add SQL schema for database

---
+++ Backend/TMDT.sql
@@ -0,0 +1,9 @@
+-- Truy vấn để kiểm tra dữ liệu
+SELECT * FROM DanhGia;
+SELECT * FROM ChiTietDonHang;
+SELECT * FROM SanPham;
+SELECT * FROM DonHang;
+SELECT * FROM LoaiSanPham;
+SELECT * FROM ThuongHieu;
+SELECT * FROM NguoiDung;
+SELECT * FROM PhanQuyen;
